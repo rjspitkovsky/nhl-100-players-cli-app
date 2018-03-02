111a74432d9424dd9db5d522b773708dcb8efc10
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-02 16:26:00 -0500

    testing bin/run lib/CLI.rb config/environment.rb bin/console

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+#require_relative '../lib/nhl_100_players_cli_app'
+require_relative '../lib/nhl_100_players_cli_app/CLI'
+
+require "bundler/setup"
+require 'nhl_100_players_cli_app'
+
+
+require 'bundler'
+Bundler.require
+require_all './lib'
---
+++ lib/nhl_100_players_cli_app/CLI.rb
@@ -0,0 +1,7 @@
+class CLI
+
+  def call
+    puts "If this works, I should be able to read this sentence."
+  end
+
+end
